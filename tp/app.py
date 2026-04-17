86b542332d5c4c0757cbe619f315edb8121e0ad4
robimuresu-sys <robimuresu@gmail.com>
2026-04-17 20:28:05 +0200

    Update app.py

--- tp/app.py
+++ tp/app.py
@@ -5,5 +5,5 @@ diff_x = st.number_input("Scostamento in x", format="%.3f")
 diff_y = st.number_input("Scostamento in y", format="%.3f")
 
 if st.button("Calcola"):
-    result = (2 * math.sqrt(((diff_x ** 2) + (diff_y ** 2))))
-    st.success(round(result), 3)
+    result = round((2 * math.sqrt(((diff_x ** 2) + (diff_y ** 2)))), 3)
+    st.success(result)
